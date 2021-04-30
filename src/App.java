9a4a06142206beeb4418b19319bc2e6c069441e8
Raynou <rafaelgame2012@gmail.com>
2021-04-30 16:24:19 -0500

    First commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Stack stack = new Stack();
+        stack.apilar(1);
+        stack.apilar(2);
+        stack.apilar(3);
+        stack.apilar(4);
+        stack.apilar(5);
+    }
+}
