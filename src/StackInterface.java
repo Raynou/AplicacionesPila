f7224a4b733341b8aaee96ba845b4b81dc4599a6
Raynou <rafaelgame2012@gmail.com>
2021-05-03 04:34:43 -0500

    Interface implementada

---
+++ src/StackInterface.java
@@ -0,0 +1,8 @@
+public interface StackInterface {
+    public Boolean isFull();
+    public Boolean isEmpty();
+    public void printStack();
+    public void reverseTheStack();
+    public void reverseTheStack(char arr[]);
+    public void print();
+}
